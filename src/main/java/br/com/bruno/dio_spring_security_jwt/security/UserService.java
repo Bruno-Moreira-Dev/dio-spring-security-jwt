b80bbbd993d73614c1ec12147b5c08eed20d6356
Bruno Pavan Adario Moreira <bpmoreira@TOPAZ.CORP>
2024-10-08 17:43:42 -0300

    feat(dio-spring-security-jwt): initial commit

---
+++ src/main/java/br/com/bruno/dio_spring_security_jwt/security/UserService.java
@@ -0,0 +1,23 @@
+package br.com.bruno.dio_spring_security_jwt.security;
+
+import br.com.bruno.dio_spring_security_jwt.model.User;
+import br.com.bruno.dio_spring_security_jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public void createUser(User user) {
+        String password = user.getPassword();
+        String encodedPassword = encoder.encode(password);
+        user.setPassword(encodedPassword);
+        repository.save(user);
+    }
+}
